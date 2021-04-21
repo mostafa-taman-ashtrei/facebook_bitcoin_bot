5d43c53665c11284bfab6740ec8636f75d5d5f82
NEO <neo.one.mail@gmail.com>
2021-04-21 08:34:45 +0200

    handle messages

--- src/controllers/chatController.ts
+++ src/controllers/chatController.ts
@@ -88,7 +88,7 @@ export const postWebhook = (req: Request, res: Response) => {
             // Check if the event is a message or postback and
             // pass the event to the appropriate handler function
             if (webhook_event.message) {
-                // handleMessage(sender_psid, webhook_event.message);
+                handleMessage(sender_psid, webhook_event.message);
             } else if (webhook_event.postback) {
                 console.log(webhook_event.postback);
                 // handlePostback(sender_psid, webhook_event.postback);
